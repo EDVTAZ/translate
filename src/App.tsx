2618882f5d1480a65c9fa83c5045f2b929d95a63
Gabor Szekely <szvgabor@gmail.com>
2025-10-24 23:04:13 +0200

    test API with fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [APIkey, setAPIkey] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <input onChange={(ev) => setAPIkey(ev.target.value)}></input>
+        <button
+          onClick={() =>
+            fetch(
+              `https://translation.googleapis.com/language/translate/v2?key=${APIkey}`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  source: "en",
+                  target: "nl",
+                  q: "hello there",
+                }),
+              }
+            ).then((response) =>
+              response.json().then((value) => console.log(value))
+            )
+          }
+        ></button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
